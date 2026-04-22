5422de2aeca0c4ae390d8fd093ad4c7b7c3bc315
i-s-d <ivan.didyk.777@gmail.com>
2026-04-22 15:09:40 +0300

    map methods

--- modlib-ifc/inc/Map.hpp
+++ modlib-ifc/inc/Map.hpp
@@ -7,24 +7,35 @@ namespace modlib {
 
 struct Vec2i { int x = 0, y = 0; };
 
+class Map;
+class Tile;
+
 class Unit {
-public:
+protected:
+    virtual Map *map() = 0;
+    virtual Tile *tile() = 0; // map().at(pos())
+
     virtual int hp() const = 0;
     virtual Vec2i pos() const = 0;
     virtual void takeDamage(int d) = 0;
-    virtual void move(Vec2i to) = 0;
-    virtual void destroy() = 0;
     virtual size_t id() = 0;
+
+    virtual void move(Vec2i to);
+    virtual void destroy();
 };
 
 class Tile {
 public:
+    virtual Vec2i pos() const;
     virtual const std::vector<Unit*> &units() = 0;
     virtual bool isWalkable() const = 0;
 };
 
 class Map : public Mod {
+    friend class Unit;
     virtual Unit *addUnit(Vec2i pos, std::unique_ptr<Unit> &&u) = 0;
+    virtual void moveUnit(Unit *u, Vec2i pos) = 0;
+    virtual void removeUnit(Unit *u) = 0;
 public:
 
     template<typename T, typename ...Args>
@@ -37,5 +48,9 @@ public:
     virtual Vec2i size() const = 0;
     virtual Tile *at(Vec2i pos) = 0;
 };
+
+
+inline void Unit::move(Vec2i to) { map()->moveUnit(this, to); }
+inline void Unit::destroy() { map()->removeUnit(this); }
 
 };
